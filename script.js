9acd7554eb79a3b3e5bee101705e68267ee09fbf
unknown <manedobolo@gmail.com>
2025-01-07 07:25:11 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggLeMode() {
+ const html = document.documentElement
+ html.classList.toggle('light')
+
+ //pegar a tag img
+ const img = document.querySelector("#profile img")
+
+ //substituir a imagem
+ if (html.classList.contains('light')) {
+ //se tiver mode,adicionar a imagem light
+ img.setAttribute("src", "./assets/avatar-light.png")
+} else {
+ //se tiver sem light mode, mater a imagem normal 
+ img.setAttribute("src", "./assets/avatar.png")
+ }
+}
